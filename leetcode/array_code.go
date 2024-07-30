8241c8a059d46ec8f0a9bb067a693e6bcf30ebee
wangyuancheng <wangyuancheng@allcam.com.cn>
2024-07-30 16:35:55 +0800

    26去除有序链表中的重复元素

---
+++ leetcode/array_code.go
@@ -0,0 +1,19 @@
+package leetcode
+
+// 26 删除有序数组中的重复项
+func removeDuplicates(nums []int) int {
+
+	if len(nums) == 0 {
+		return 0
+	}
+
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != nums[slow] {
+			slow++
+			nums[slow] = nums[fast]
+		}
+		fast++
+	}
+	return slow + 1
+}

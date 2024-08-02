203d5badfd3a2a648dfd2e4981b0c06bd50efcda
wangyuancheng <wangyuancheng@allcam.com.cn>
2024-08-02 22:38:57 +0800

    76 最小覆盖子串

--- leetcode/slidewindow/slide_window.go
+++ leetcode/slidewindow/slide_window.go
@@ -3,4 +3,57 @@ package slidewindow
 // 76 最小覆盖子串
 func minWindow(s string, t string) string {
 
+	need := map[rune]int{}
+	window := map[rune]int{}
+	for _, t0 := range t {
+		need[t0] += 1
+	}
+
+	var res string
+
+	left, right := 0, 0
+	for right < len(s) {
+		if _, ok := need[rune(s[right])]; ok {
+			window[rune(s[right])] += 1
+		}
+		// 判断此时窗口内的元素是否已经满足t
+		match := matchWindow(need, window)
+		// 缩小窗口
+		for match {
+
+			sub := s[left : right+1]
+			if res == "" {
+				res = sub
+			} else if len(sub) < len(res) {
+				res = sub
+			}
+
+			if _, exist := window[rune(s[left])]; !exist {
+				left++
+				continue
+			}
+
+			// 缩小窗口
+			window[rune(s[left])]--
+			left++
+			// 比较是否仍满足
+			match = matchWindow(need, window)
+
+		}
+		right++
+	}
+
+	return res
+}
+
+func matchWindow(need, window map[rune]int) bool {
+	if len(window) < len(need) {
+		return false
+	}
+	for k, v := range need {
+		if v > window[k] {
+			return false
+		}
+	}
+	return true
 }
--- leetcode/slidewindow/slide_window_test.go
+++ leetcode/slidewindow/slide_window_test.go
@@ -5,12 +5,50 @@ import (
 )
 
 func TestMinWindow(t *testing.T) {
-	s := "ADOBECODEBANC"
-	su := "ABC"
-	t.Log(minWindow(s, su) == "BANC")
 
-	s = "aa"
-	su = "aa"
-	t.Log(minWindow(s, su) == "aa")
+	type field struct {
+		s, t string
+	}
+
+	tests := []struct {
+		name string
+		f    field
+	}{
+		{
+			name: "case1",
+			f: field{
+				s: "ADOBECODEBANC",
+				t: "ABC",
+			},
+		},
+		{
+			"case2",
+			field{
+				s: "aa",
+				t: "aa",
+			},
+		},
+		{
+			"case3",
+			field{
+				s: "a",
+				t: "a",
+			},
+		},
+		{
+			"case4",
+			field{
+				s: "bba",
+				t: "ab",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			t.Log(minWindow(test.f.s, test.f.t))
+		})
+	}
 
 }

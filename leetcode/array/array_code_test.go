761fd57d510972ed182c11008c473541f0fa85ec
wangyuancheng <wangyuancheng@allcam.com.cn>
2024-09-25 21:16:53 +0800

    1260 二维网格迁移

--- leetcode/array/array_code_test.go
+++ leetcode/array/array_code_test.go
@@ -2,6 +2,7 @@ package array
 
 import (
 	"log"
+	"reflect"
 	"regexp"
 	"strings"
 	"testing"
@@ -49,4 +50,68 @@ func TestRemoveSliceFirst(t *testing.T) {
 	arr := []int{1, 2, 3, 4, 5}
 	arr = append(arr[1:])
 	log.Println(arr)
+
+	m := make(map[int][]int)
+	ints := m[3]
+	log.Println(len(ints))
+
+	var a []int
+	log.Println(len(a))
+	log.Println(a == nil)
+
+}
+
+func Test_sortedSquares(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"case1", args{[]int{-4, -1, 0, 3, 10}}, []int{0, 1, 9, 16, 100}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortedSquares(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortedSquares() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_sortTransformedArray(t *testing.T) {
+	type args struct {
+		nums []int
+		a    int
+		b    int
+		c    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"case1", args{[]int{-4, -2, 2, 4}, 1, 3, 5}, []int{3, 9, 15, 33}},
+		{"case2", args{[]int{-4, -2, 2, 4}, -1, 3, 5}, []int{-23, -5, 1, 7}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortTransformedArray(tt.args.nums, tt.args.a, tt.args.b, tt.args.c); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortTransformedArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRotate(t *testing.T) {
+
+	arr := []int{1, 2, 3, 4, 5}
+	k := 3
+	target := make([]int, len(arr))
+	for i := range arr {
+		target[(i+k)%len(arr)] = arr[i]
+	}
+	log.Println(target)
 }
--- leetcode/linkedlist/linkedlist_code_test.go
+++ leetcode/linkedlist/linkedlist_code_test.go
@@ -2,7 +2,6 @@ package linkedlist
 
 import (
 	"log"
-	"reflect"
 	"testing"
 )
 
@@ -145,48 +144,4 @@ func Test_isPalindrome(t *testing.T) {
 	n3.Next = n4
 	t.Log(isPalindrome(n1))
 
-}
-
-func Test_sortedSquares(t *testing.T) {
-	type args struct {
-		nums []int
-	}
-	tests := []struct {
-		name string
-		args args
-		want []int
-	}{
-		{"case1", args{[]int{-4, -1, 0, 3, 10}}, []int{0, 1, 9, 16, 100}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := sortedSquares(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("sortedSquares() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_sortTransformedArray(t *testing.T) {
-	type args struct {
-		nums []int
-		a    int
-		b    int
-		c    int
-	}
-	tests := []struct {
-		name string
-		args args
-		want []int
-	}{
-		{"case1", args{[]int{-4, -2, 2, 4}, 1, 3, 5}, []int{3, 9, 15, 33}},
-		{"case2", args{[]int{-4, -2, 2, 4}, -1, 3, 5}, []int{-23, -5, 1, 7}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := sortTransformedArray(tt.args.nums, tt.args.a, tt.args.b, tt.args.c); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("sortTransformedArray() = %v, want %v", got, tt.want)
-			}
-		})
-	}
 }
